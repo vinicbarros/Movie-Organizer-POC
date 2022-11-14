92fbc49cbad52e7ae2adea33f953d9f180bb0368
Vinícius Barros <vicaetano_barros@hotmail.com>
2022-11-14 18:04:41 -0300

    fix: fixed router

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { json } from "express";
 import cors from "cors";
 import moviesRouter from "./routers/moviesRouter.js";
+import platformRouter from "./routers/platformRouters.js";
+import genderRouter from "./routers/genderRouter.js";
 
 const app = express();
 
 app.use(cors());
 app.use(json());
 app.use(moviesRouter);
+app.use(platformRouter);
+app.use(genderRouter);
 
 export default app;
